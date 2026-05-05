7210986336ac483be12fc40f8cfe0a4b87e379f7
Vikas Gujjar <www.vgujjar1234@gmail.com>
2026-05-05 12:21:09 +0530

    genereate build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
